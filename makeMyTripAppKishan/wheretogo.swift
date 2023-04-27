0b58472d55554dc8d856db583b0e8042a13820f7
Kishan Govindiya <kishangovindiya1@gmail.com>
2023-04-28 00:50:42 +0530

    28April

---
+++ makeMyTripAppKishan/wheretogo.swift
@@ -0,0 +1,18 @@
+//
+//  wheretogo.swift
+//  makeMyTripAppKishan
+//
+//  Created by R82 on 27/04/23.
+//
+
+import UIKit
+
+class wheretogo: UIViewController {
+
+    var arr = [21,22,23,24,25]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+ 
+}
